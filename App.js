e33eeb03796a86e245ab72011beb4a1ce9fa014f
mohamedebrahimkhatab <mohamedebrahimkhatab@gmail.com>
2023-04-16 09:40:46 +0200

    layout

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { Platform, SafeAreaView, StyleSheet, Text, View, StatusBar } from 'react-native';
+
+const isAndroid = Platform.OS == 'android'
 
 export default function App() {
   return (
-    <View style={{ flex:1, alignItems:"center", justifyContent:"center" }}>
-      <Text>our react native blank canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex:1, marginTop: StatusBar.currentHeight }}>
+        <View style={{ padding: 16, backgroundColor:"green" }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex:1, padding:16, backgroundColor:"blue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
