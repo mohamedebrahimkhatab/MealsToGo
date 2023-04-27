d17e9bd62137504d374c4f60551cd0f15330fc38
mohamedebrahimkhatab <mohamedebrahimkhatab@gmail.com>
2023-04-27 04:54:06 +0200

    adding theme

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastructure/theme";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantScreen />
+      </ThemeProvider>
       <StatusBar style="auto" />
     </>
   );
