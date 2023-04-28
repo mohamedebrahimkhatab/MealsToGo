3c2df0d402f3902a704a99c882cf24bfa9be6cfd
mohamedebrahimkhatab <mohamedebrahimkhatab@gmail.com>
2023-04-29 01:40:46 +0300

    fix android bug

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -84,10 +84,12 @@ export const RestautrantInfoCard = (restaurant = {}) => {
                 CLOSED TEMPORARILY
               </Text>
             )}
-            <Spacer position="left" size="large" />
-            {isOpenNow && <Open xml={open} width={20} height={20} />}
-            <Spacer position="left" size="large" />
-            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            <Spacer position="left" size="large">
+              {isOpenNow && <Open xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
           </SectionEnd>
         </Section>
         <Address>{address}</Address>
